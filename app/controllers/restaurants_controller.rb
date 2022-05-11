42dcbc0f141f8bcdd2a1c3b572c4239bad1dd38a
Sarah Hall <swatson1994@yahoo.com>
2022-05-11 17:18:11 -0400

    "Add show action to restaurant controller"

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,8 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show #Can add these to application controller if reused?
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
