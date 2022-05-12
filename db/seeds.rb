edb7f18194768528d59e9d96888d54df2ea0e377
Sarah Hall <swatson1994@yahoo.com>
2022-05-12 16:38:48 -0400

    "Add repeated test set up to seed"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dish.destroy_all
+Restaurant.destroy_all
+
+@billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
+@flapjack = Restaurant.create!(name: "Flapjack's", open: true, guest_capacity: 105)
+@fry_pickle = @billy.dishes.create!(name: "Fried Pickles", in_season: true, cost: 8)
+@banana_pud = @flapjack.dishes.create!(name: "Banana Pudding", in_season: false, cost: 5)
