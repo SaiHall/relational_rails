c8f7d78666897278f1002c87b7e7b4549dc3c000
Sarah Hall <swatson1994@yahoo.com>
2022-05-12 17:02:58 -0400

    "Test: Add test for created at timestamp on restaurant index page"

--- spec/features/restaurants/index_spec.rb
+++ spec/features/restaurants/index_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "restaurants index page", type: :feature do
-  it "can see all restaurant names" do
-    restaurant_1 = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
-    restaurant_2 = Restaurant.create!(name: "Flapjack's", open: true, guest_capacity: 105)
+  before(:each) do
+    @billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
+    @flapjack = Restaurant.create!(name: "Flapjack's", open: true, guest_capacity: 105)
 
     visit "/restaurants"
-
-    expect(page).to have_content(restaurant_1.name)
-    expect(page).to have_content(restaurant_2.name)
   end
+
+  it "can see all restaurant names" do
+
+    expect(page).to have_content(@billy.name)
+    expect(page).to have_content(@flapjack.name)
+  end
+
+  it 'displays when a restaurant was created' do
+
+    expect(page).to have_content(@billy.created_at)
+    expect(page).to have_content(@flapjack.created_at)
+  end
+
+  it 'displays restaurants in the descending order from when they were created'
 end
