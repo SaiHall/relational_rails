3d404c72780573729ea29528e7655fbff0cab27d
Sarah Hall <swatson1994@yahoo.com>
2022-05-12 16:32:34 -0400

    "Test: Add model tests for relationship and validations"

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  describe 'instance methods' do
-
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :guest_capacity}
+    it { should allow_value(true).for(:open)}
+    it { should allow_value(false).for(:open)}
   end
+
+  describe 'relationships' do
+    it {should have_many :dishes}
+  end
+
 end
