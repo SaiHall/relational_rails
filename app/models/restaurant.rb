46dbbdeb40c73379db9b5b3df794109fe89dd324
Sarah Hall <swatson1994@yahoo.com>
2022-05-13 17:32:27 -0400

    "Feat: Add class method to put restaurants in correct order"

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,8 @@ class Restaurant < ApplicationRecord
 
   validates_presence_of :name, :guest_capacity
   validates :open, inclusion: [ true, false ]
+
+  def self.newest_first
+    order(created_at: :desc)
+  end
 end
