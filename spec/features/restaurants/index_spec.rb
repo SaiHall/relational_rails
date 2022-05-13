4d019b3e5c14297acd5b1110e9f38a518589bb97
Sarah Hall <swatson1994@yahoo.com>
2022-05-13 16:39:56 -0400

    "Test: Add test for display order on restaurant index page"

--- spec/features/restaurants/index_spec.rb
+++ spec/features/restaurants/index_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe "restaurants index page", type: :feature do
     expect(page).to have_content(@billy.name)
     expect(page).to have_content(@flapjack.name)
   end
+# As a visitor
+# When I visit the parent index,
+# I see that records are ordered by most recently created first
+# And next to each of the records I see when it was created
 
   it 'displays when a restaurant was created' do
 
@@ -20,5 +24,10 @@ RSpec.describe "restaurants index page", type: :feature do
     expect(page).to have_content(@flapjack.created_at)
   end
 
-  it 'displays restaurants in the descending order from when they were created'
+  it 'displays restaurants in the descending order from when they were created' do
+    within '#restaurantIndex' do
+      expect(page.all('.restaurant')[0]).to have_content("Flapjack's")
+      expect(page.all('.restaurant')[1]).to have_content("Billy's BBQ Bodega")
+    end
+  end
 end
