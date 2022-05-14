28e00c60dd831ef7d950fe4ad7dccf2de404d278
Sarah Hall <swatson1994@yahoo.com>
2022-05-14 16:00:41 -0400

    "Test: Add test for functioning links from rest index"

--- spec/features/restaurants/index_spec.rb
+++ spec/features/restaurants/index_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe "restaurants index page", type: :feature do
   before(:each) do
     @billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
     @flapjack = Restaurant.create!(name: "Flapjack's", open: true, guest_capacity: 105)
-
+    @fry_pickle = @billy.dishes.create!(name: "Fried Pickles", in_season: true, cost: 8)
+    @banana_pud = @flapjack.dishes.create!(name: "Banana Pudding", in_season: false, cost: 5)
     visit "/restaurants"
   end
 
@@ -28,6 +29,24 @@ RSpec.describe "restaurants index page", type: :feature do
     within '#restaurantIndex' do
       expect(page.all('.restaurant')[0]).to have_content("Flapjack's")
       expect(page.all('.restaurant')[1]).to have_content("Billy's BBQ Bodega")
+    end
+  end
+
+  describe 'Functioning links' do
+    it 'has a working link to the dishes index' do
+      visit "/restaurants"
+      expect(page).to have_content("All Dishes")
+      click_on('All Dishes')
+      expect(page).to have_current_path("/dishes")
+      expect(page).to have_content("Fried Pickles")
+    end
+
+    it 'has a working link to the restaurants index' do
+      visit "/restaurants"
+      expect(page).to have_content("All Restaurants")
+      click_on('All Restaurants')
+      expect(page).to have_current_path("/restaurants")
+      expect(page).to have_content("Flapjack's")
     end
   end
 end
