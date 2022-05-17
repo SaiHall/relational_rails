1beab5a88dae3f8d6b708ae88f376eb9b6bd8b22
Sarah Hall <swatson1994@yahoo.com>
2022-05-16 23:14:19 -0400

    "Add private params and new/create methods to rest/dish/controller"

--- app/controllers/restaurant_dishes_controller.rb
+++ app/controllers/restaurant_dishes_controller.rb
@@ -3,4 +3,18 @@ class RestaurantDishesController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @dishes = @restaurant.dishes
   end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    Dish.create(rest_dish_params)
+    redirect_to "/restaurants/#{params[:restaurant_id]}/dishes"
+  end
+
+  private
+    def rest_dish_params
+      params.permit(:name, :in_season, :cost, :restaurant_id)
+    end
 end
