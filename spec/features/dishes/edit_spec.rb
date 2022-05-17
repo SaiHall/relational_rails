cbe13eafbaeddfc6911dae71b52014f0ce20f6ff
Sarah Hall <swatson1994@yahoo.com>
2022-05-16 23:31:05 -0400

    "Test: Add tests for dish edit page/function"

---
+++ spec/features/dishes/edit_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "dishes edit page", type: :feature do
+  before(:each) do
+    @billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
+    @fry_pickle = @billy.dishes.create!(name: "Fried Pickles", in_season: true, cost: 8)
+  end
+  describe 'Dish update' do
+    it 'has a link to the update page from the show page' do
+      visit "/dishes/#{@fry_pickle.id}"
+
+      click_link("Update Dish")
+
+      expect(page).to have_current_path("/dishes/#{@fry_pickle.id}/edit")
+    end
+
+    it 'has a form that can be filled out to update a restaurant entry' do
+      visit("/dishes/#{@fry_pickle.id}/edit")
+
+      fill_in 'Name', with: "Fried Cauliflower"
+      choose :in_season, with: false #radio button
+      fill_in 'Cost', with: '11'
+
+      click_button("Update Dish")
+
+      expect(page).to have_current_path("/dishes/#{@fry_pickle.id}")
+      expect(page).to have_content("Fried Cauliflower")
+      expect(page).to have_content("Currently in season: false")
+      expect(page).to have_content("Cost: $11")
+    end
+  end
+end
