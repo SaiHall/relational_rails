fe030b7a61d21b3a669e4b54fce5f498ef991b97
Sarah Hall <swatson1994@yahoo.com>
2022-05-16 21:31:03 -0400

    "Test: Add tests for update for for specific restaurants"

--- spec/features/restaurants/edit_spec.rb
+++ spec/features/restaurants/edit_spec.rb
@@ -13,5 +13,21 @@ RSpec.describe "restaurants edit page", type: :feature do
 
       expect(page).to have_current_path("/restaurants/#{@billy.id}/edit")
     end
+
+    it 'has a form that can be filled out to update a restaurant entry' do
+      visit("/restaurants/#{@billy.id}/edit")
+
+      fill_in 'Name', with: "William's BBQ Bodega"
+      choose :open, with: false #radio button
+      fill_in 'Guest capacity', with: '55'
+
+      click_button("Update Restaurant")
+
+      expect(page).to have_current_path("/restaurants/#{@billy.id}")
+      expect(page).to have_content("William's BBQ Bodega")
+      expect(page).to have_content("Open for business: false")
+      expect(page).to have_content("Capacity: 55")
+      # expect(@billy.created_at).to_not eq(@billy.updated_at) Why does updated at not change?
+    end
   end
 end
