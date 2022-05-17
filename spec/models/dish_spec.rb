dbb56eaf538aeb3e2d91de30494205797408d49a
Sarah Hall <swatson1994@yahoo.com>
2022-05-17 17:21:49 -0400

    "Test: Add model test for alphabetized dish method"

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -28,5 +28,17 @@ RSpec.describe Dish, type: :model do
       expect(seasonal).to eq([fry_pickle, waffle])
       expect(seasonal.by_in_season).to eq([fry_pickle])
     end
+
+    it 'can alphabetize dishes' do
+      billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
+      flapjack = Restaurant.create!(name: "Flapjack's", open: true, guest_capacity: 105)
+      fry_pickle = billy.dishes.create!(name: "Fried Pickles", in_season: true, cost: 8)
+      banana_pud = flapjack.dishes.create!(name: "Banana Pudding", in_season: false, cost: 5)
+      waffle = flapjack.dishes.create!(name: "Belgian Bonanza", in_season: false, cost: 13)
+
+      menu = Dish.all
+
+      expect(menu.alphabetize).to eq([banana_pud, waffle, fry_pickle])
+    end
   end
 end
