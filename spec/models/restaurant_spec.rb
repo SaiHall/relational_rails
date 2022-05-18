17f7071f66735fc0b3aef9ed79b2ba28adb6c221
Sarah Hall <swatson1994@yahoo.com>
2022-05-18 17:06:58 -0400

    "Test: Add tests class methods, dish filtered_by and rest.filtered"

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -19,6 +19,17 @@ RSpec.describe Restaurant, type: :model do
       expect(Restaurant.all.newest_first[0].name).to eq("Flapjack's")
       expect(Restaurant.all.newest_first[1].name).to eq("Billy's BBQ Bodega")
     end
+
+    it 'can sort children based off parameters, and give default if none provided' do
+      billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
+      fry_pickle = billy.dishes.create!(name: "Fried Pickles", in_season: true, cost: 8)
+      banana_pud = billy.dishes.create!(name: "Banana Pudding", in_season: false, cost: 5)
+      waffle = billy.dishes.create!(name: "Belgian Bonanza", in_season: false, cost: 13)
+
+      expect(billy.filtered(nil, 10)).to eq([waffle])
+      expect(billy.filtered("alphabetically")).to eq([banana_pud, waffle, fry_pickle])
+      expect(billy.filtered).to eq([fry_pickle, banana_pud, waffle])
+    end
   end
   describe 'instance methods' do
     before(:each) do
