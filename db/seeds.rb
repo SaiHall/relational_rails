98d3dd03580a0ede3435f585adb74c3ece46886e
Sarah Hall <swatson1994@yahoo.com>
2022-05-18 18:52:26 -0400

    "Fill in seed with more options"

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,13 @@ Restaurant.destroy_all
 
 @billy = Restaurant.create!(name: "Billy's BBQ Bodega", open: true, guest_capacity: 35)
 @flapjack = Restaurant.create!(name: "Flapjack's", open: true, guest_capacity: 105)
+
 @fry_pickle = @billy.dishes.create!(name: "Fried Pickles", in_season: true, cost: 8)
-@banana_pud = @flapjack.dishes.create!(name: "Banana Pudding", in_season: false, cost: 5)
+@salad = @billy.dishes.create!(name: "Sensational Steak Salad", in_season: true, cost: 12)
+@toast = @billy.dishes.create!(name: "Texas-style Toast", in_season: false, cost: 6)
+@ribs = @billy.dishes.create!(name: "Ribs - Bone-in", in_season: true, cost: 17)
+
+@banana_pud = @flapjack.dishes.create!(name: "Banana Pudding", in_season: true, cost: 5)
+@sunrise = @flapjack.dishes.create!(name: "Sunrise Cider-cakes", in_season: true, cost: 15)
+@pancake = @flapjack.dishes.create!(name: "Princess Peach Pancake Pucker", in_season: true, cost: 11)
+@melon = @flapjack.dishes.create!(name: "Mouth-watering Mound of Melon", in_season: false, cost: 9)
